100a565283a3953e2e7d7cc2cede3d3d6d87f50c
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2024-06-23 23:43:48 +0530

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,26 @@
 const SideBar = ({ activeTab, posts, handleTab, handleAddJob }) => {
-    return <aside className="w-1/5">
-        <button onClick={handleAddJob} className="px-4 py-2 bg-white border-2 border-gray-500 w-full">New Job +</button>
-        <ul>
-            {posts.length > 0 ? (
-                posts.map((post, i) => <li key={i} onClick={() => handleTab(post.id, i)} className={`${activeTab === i ? 'bg-sky-400' : 'bg-gray-300'}  p-2 border-b-2 border-slate-500 cursor-pointer`}>{`${post.title.title} (Post ${i + 1})`}</li>)
-            ) : null}
-
-        </ul>
-    </aside>
-}
+    return (
+        <aside className="w-full lg:w-1/5">
+            <button
+                onClick={handleAddJob}
+                className="px-4 py-2 bg-white border-2 border-gray-500 w-full"
+            >
+                New Job +
+            </button>
+            <ul>
+                {posts.length > 0
+                    ? posts.map((post, i) => (
+                        <li
+                            key={i}
+                            onClick={() => handleTab(post.id, i)}
+                            className={`${activeTab === i ? "bg-sky-400" : "bg-gray-300"
+                                }  p-2 border-b-2 border-slate-500 cursor-pointer`}
+                        >{`${post.title.title} (Post ${i + 1})`}</li>
+                    ))
+                    : null}
+            </ul>
+        </aside>
+    );
+};
 
 export default SideBar;
