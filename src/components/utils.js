c5d2708a51b3f8d488591f68dc1ed46e87034162
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2024-06-23 23:44:03 +0530

    Update utils.js

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,6 +1,20 @@
-export const setDataInLS = (key, value) => {
-  return localStorage.setItem(key, JSON.stringify(value));
+import { LS_POST_MANAGER_KEY } from "./constants";
+
+export const setDataInLS = (value) => {
+  if (value) {
+    return localStorage.setItem(LS_POST_MANAGER_KEY, JSON.stringify(value));
+  } else {
+    return null;
+  }
 };
-export const getDataFromLS = (key) => {
-  return getDataFromLS(JSON.parse(key));
+
+export const getDataFromLS = () => {
+  const lsPostManagerData = JSON.parse(
+    localStorage.getItem(LS_POST_MANAGER_KEY)
+  );
+  if (lsPostManagerData) {
+    return lsPostManagerData;
+  } else {
+    return [];
+  }
 };
