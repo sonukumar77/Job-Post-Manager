fc599d2493de02f713eb22f8bd82768d83192688
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2024-06-23 23:44:21 +0530

    Update Footer.jsx

--- src/components/reusable/Footer.jsx
+++ src/components/reusable/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-400 text-black text-center ">
+        <footer className="bg-gray-400 text-black text-center w-full fixed bottom-0">
             <p className="text-2xl">All Right Reserved &reg;2024</p>
             <p>Made with ❤️ by Sonu</p>
         </footer>
