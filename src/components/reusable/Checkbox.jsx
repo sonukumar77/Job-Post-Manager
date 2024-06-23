d509cc28eeeefbdadc65c8964c7422f1dc8d5eba
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2024-06-23 23:46:33 +0530

    Delete src/components/reusable/Checkbox.jsx

--- src/components/reusable/Checkbox.jsx
+++
@@ -1,7 +0,0 @@
-const Checkbox = ({ isChecked, handleCheckbox }) => {
-    return (
-        <input checked={isChecked} onChange={(e) => handleCheckbox(e)} type="checkbox" className="mr-4 w-6 h-6" name="title" id="title" />
-    )
-}
-
-export default Checkbox
