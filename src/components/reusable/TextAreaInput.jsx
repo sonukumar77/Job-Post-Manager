65fcd591c6fef46aad7e666c43bbb478bb34fa07
Sonu Kumar <82059136+sonukumar77@users.noreply.github.com>
2024-06-23 23:46:47 +0530

    Delete src/components/reusable/TextAreaInput.jsx

--- src/components/reusable/TextAreaInput.jsx
+++
@@ -1,7 +0,0 @@
-const TextArea = ({ textareaValue, handleInput }) => {
-    return (
-        <textarea value={textareaValue} onChange={(e) => handleInput(e)} name="intro" id="intro" className="border-2 border-slate-400 p-2 w-full" cols="60" rows="2" placeholder="The ideal candidate is someone..."></textarea>
-    )
-}
-
-export default TextArea
